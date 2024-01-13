4f9f798797c4e6277ca62d95bb9f2388b5a510e4
Tranquananh2811 (Jack) <84040304+Tranquananh2811@users.noreply.github.com>
2024-01-12 22:13:54 -0400

    Update TableLibraryModule.lua

--- TableLibraryModule.lua
+++ TableLibraryModule.lua
@@ -1,156 +1,162 @@
 return {
-    clear = function(tbl)
-        return {}
-    end,
-    
-    clone = function(tbl)
-        local function cloneFunc(t)
-            local clone = {}
-            for i, v in pairs(tbl) do
-                if type(v) == "table" then
-                    clone[i] = cloneFunc(v)
-                else
-                    clone[i] = v
-                end
-            end
-            return clone
-        end
+		clear = function(tbl)
+				return {}
+		end,
 
-        return cloneFunc(tbl)
-    end,
+		clone = function(tbl)
+				local function cloneFunc(t)
+						local function IsTable(a)
+								local succ, fail = pcall(function()
+										a = #a
+								end)
+								return succ
+						end
+						local clone = {}
+						for i, v in pairs(tbl) do
+								if IsTable(v) then
+										clone[i] = cloneFunc(v)
+								else
+										clone[i] = v
+								end
+						end
+						return clone
+				end
 
-    sort = function(tbl, compareFunc)
-        local n = #tbl
-        compareFunc = compareFunc or function(a, b)
-                return a < b
-            end
+				return cloneFunc(tbl)
+		end,
 
-        for i = 1, n - 1 do
-            for j = 1, n - i do
-                if compareFunc(tbl[j + 1], tbl[j]) then
-                    tbl[j], tbl[j + 1] = tbl[j + 1], tbl[j]
-                end
-            end
-        end
-    end,
+		sort = function(tbl, compareFunc)
+				local n = #tbl
+				compareFunc = compareFunc or function(a, b)
+								return a < b
+						end
 
-    insert = function(tbl, value, pos)
-        pos = pos or (#tbl + 1)
+				for i = 1, n - 1 do
+						for j = 1, n - i do
+								if compareFunc(tbl[j + 1], tbl[j]) then
+										tbl[j], tbl[j + 1] = tbl[j + 1], tbl[j]
+								end
+						end
+				end
+		end,
 
-        for i = #tbl, pos, -1 do
-            tbl[i + 1] = tbl[i]
-        end
+		insert = function(tbl, value, pos)
+				pos = pos or (#tbl + 1)
 
-        tbl[pos] = value
-    end,
+				for i = #tbl, pos, -1 do
+						tbl[i + 1] = tbl[i]
+				end
 
-    find = function(tbl, value)
-        for i, v in ipairs(tbl) do
-            if v == value then
-                return i
-            end
-        end
-        return nil
-    end,
+				tbl[pos] = value
+		end,
 
-    remove = function(tbl, pos)
-        pos = pos or #tbl
+		find = function(tbl, value)
+				for i, v in ipairs(tbl) do
+						if v == value then
+								return i
+						end
+				end
+				return nil
+		end,
 
-        local removedValue = tbl[pos]
+		remove = function(tbl, pos)
+				pos = pos or #tbl
 
-        for i = pos, #tbl - 1 do
-            tbl[i] = tbl[i + 1]
-        end
+				local removedValue = tbl[pos]
 
-        tbl[#tbl] = nil
+				for i = pos, #tbl - 1 do
+						tbl[i] = tbl[i + 1]
+				end
 
-        return removedValue
-    end,
+				tbl[#tbl] = nil
 
-    foreach = function(tbl, callback)
-        print("foreach is deprecated, please use pairs loop!!")
-        for i, v in pairs(tbl) do
-            callback(i, v)
-        end
-    end,
+				return removedValue
+		end,
 
-    foreachi = function(tbl, callback)
-        print("foreachi is deprecated, please use ipairs loop!!")
-        for i, v in ipairs(tbl) do
-            callback(i, v)
-        end
-    end,
+		foreach = function(tbl, callback)
+				print("foreach is deprecated, please use pairs loop!!")
+				for i, v in pairs(tbl) do
+						callback(i, v)
+				end
+		end,
 
-    concat = function(tbl, sep, i, j)
-        sep = sep or ""
-        i = i or 1
-        j = j or #tbl
+		foreachi = function(tbl, callback)
+				print("foreachi is deprecated, please use ipairs loop!!")
+				for i, v in ipairs(tbl) do
+						callback(i, v)
+				end
+		end,
 
-        local result = ""
-        for k = i, j do
-            result = result .. tostring(tbl[k]) .. (k < j and sep or "")
-        end
+		concat = function(tbl, sep, i, j)
+				sep = sep or ""
+				i = i or 1
+				j = j or #tbl
 
-        return result
-    end,
+				local result = ""
+				for k = i, j do
+						result = result .. tostring(tbl[k]) .. (k < j and sep or "")
+				end
 
-    pack = function(...)
-        local result = {}
-        local arg = {...}
-        result.n = #arg
-        for i = 1, result.n do
-            result[i] = arg[i]
-        end
-        return result
-    end,
+				return result
+		end,
 
-    unpack = function(tbl, i, j)
-        i = i or 1
-        j = j or #tbl
+		pack = function(...)
+				local result = {}
+				local arg = {...}
+				result.n = #arg
+				for i = 1, result.n do
+						result[i] = arg[i]
+				end
+				return result
+		end,
 
-        local function unpackHelper(tbl, index, last)
-            if index > last then
-                return
-            else
-                return tbl[index], unpackHelper(tbl, index + 1, last)
-            end
-        end
+		unpack = function(tbl, i, j)
+				i = i or 1
+				j = j or #tbl
 
-        return unpackHelper(tbl, i, j)
-    end,
+				local function unpackHelper(tbl, index, last)
+						if index > last then
+								return
+						else
+								return tbl[index], unpackHelper(tbl, index + 1, last)
+						end
+				end
 
-    move = function(src, from, to, dest)
-        dest = dest or src
-        local moved = {}
-        local j = 1
+				return unpackHelper(tbl, i, j)
+		end,
 
-        for i = from, to do
-            dest[j] = src[i]
-            moved[j] = dest[j]
-            j = j + 1
-        end
+		move = function(src, from, to, dest)
+				dest = dest or src
+				local moved = {}
+				local j = 1
 
-        for i = from, to do
-            src[i] = nil
-        end
+				for i = from, to do
+						dest[j] = src[i]
+						moved[j] = dest[j]
+						j = j + 1
+				end
 
-        return moved
-    end,
+				for i = from, to do
+						src[i] = nil
+				end
 
-    getn = function(tbl)
-        print("table.getn is deprecated, please use #tbl to get the length!!")
-        return #tbl
-    end,
+				return moved
+		end,
 
-    maxn = function(tbl)
-        local maxIndex = 0
+		getn = function(tbl)
+				print("table.getn is deprecated, please use #tbl to get the length!!")
+				return #tbl
+		end,
 
-        for i, v in pairs(tbl) do
-            if tonumber(i) ~= nil and i > maxIndex and math.floor(i) == i and i > 0 then
-                maxIndex = i
-            end
-        end
+		maxn = function(tbl)
+				local maxIndex = 0
 
-        return maxIndex
-    end
+				for i, v in pairs(tbl) do
+						if tonumber(i) ~= nil and i > maxIndex and math.floor(i) == i and i > 0 then
+								maxIndex = i
+						end
+				end
+
+				return maxIndex
+		end
 }
